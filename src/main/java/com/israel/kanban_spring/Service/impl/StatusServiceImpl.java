9b996945a91e27fce70eae39ec1df55e0293ad89
israel <israelmessias2016@outlook.com>
2022-06-24 19:53:08 -0300

    Service Status  concluido

--- src/main/java/com/israel/kanban_spring/Service/impl/StatusServiceImpl.java
+++ src/main/java/com/israel/kanban_spring/Service/impl/StatusServiceImpl.java
@@ -4,9 +4,10 @@ import com.israel.kanban_spring.Service.interfaces.StatusService;
 import com.israel.kanban_spring.model.entity.Status;
 import com.israel.kanban_spring.model.repository.StatusRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
-
+@Service
 public class StatusServiceImpl implements StatusService {
     @Autowired
     StatusRepository repository;
