b2840e6efd501053f2acaf63cc0e8bb7df553ae5
israel <israelmessias2016@outlook.com>
2022-06-24 20:03:27 -0300

    repository do projeto

--- src/main/java/com/israel/kanban_spring/model/repository/ProjetoRepository.java
+++ src/main/java/com/israel/kanban_spring/model/repository/ProjetoRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.israel.kanban_spring.model.entity.Projeto;
 
 public interface ProjetoRepository extends JpaRepository<Projeto, Integer> {
-    
+
 }
