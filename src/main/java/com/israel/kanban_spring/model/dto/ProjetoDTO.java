c6e277f438adef9992290b23d96eaecb1b01e531
israel <israelmessias2016@outlook.com>
2022-06-24 19:58:34 -0300

    DTO do Projeto

--- src/main/java/com/israel/kanban_spring/model/dto/ProjetoDTO.java
+++ src/main/java/com/israel/kanban_spring/model/dto/ProjetoDTO.java
@@ -1,2 +1,17 @@
-package com.israel.kanban_spring.model.dto;public class ProjetoDto {
+package com.israel.kanban_spring.model.dto;
+
+import lombok.Data;
+
+import java.util.List;
+import java.util.Set;
+
+@Data
+public class ProjetoDTO {
+    private Integer id;
+
+    private String titulo;
+
+    private String descricao;
+
+    private Set<Integer> tarefas;
 }
