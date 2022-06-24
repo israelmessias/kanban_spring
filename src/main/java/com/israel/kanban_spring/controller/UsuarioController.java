bcc2f1bed0d5e7495479b4effa991862fda57751
israel <israelmessias2016@outlook.com>
2022-06-24 20:00:45 -0300

    Controller do Usuario

--- src/main/java/com/israel/kanban_spring/controller/UsuarioController.java
+++ src/main/java/com/israel/kanban_spring/controller/UsuarioController.java
@@ -4,13 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.israel.kanban_spring.Service.impl.UsuarioServiceImpl;
 import com.israel.kanban_spring.model.dto.UsuarioDTO;
@@ -36,7 +30,7 @@ public class UsuarioController {
             return new ResponseEntity(usuarioSalvo, HttpStatus.CREATED);
         } catch (Exception e) {
             //TODO: handle exception
-            System.out.println(e.getMessage());
+
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
@@ -52,7 +46,7 @@ public class UsuarioController {
                 return new ResponseEntity(usuario, HttpStatus.CREATED);
             } catch (Exception e) {
                 //TODO: handle exception
-                System.out.println("!");
+
                 return ResponseEntity.badRequest().body(e.getMessage());
             }
         }).orElseGet(() -> new ResponseEntity("Não encontrado na base de dados", HttpStatus.BAD_REQUEST));
@@ -62,12 +56,23 @@ public class UsuarioController {
     public ResponseEntity autenticar(@RequestBody UsuarioDTO dto){
         try {
           Usuario  usuarioAutenticado = service.autenticar(dto.getEmail(), dto.getSenha());
-            System.out.println("passou");
+
             return ResponseEntity.ok(usuarioAutenticado);
         } catch (Exception e) {
             return ResponseEntity.badRequest().body("menssagem: "+e.getMessage());
         }
 
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity deletarUsuario(@PathVariable Integer id){
+        return service.obterPorId(id).map(entity ->
+        {
+            service.deletar(entity);
+            return new ResponseEntity(HttpStatus.NO_CONTENT);
+        }).orElseGet(
+                () -> new ResponseEntity("Usuario nao encontrado na base de dados",
+                        HttpStatus.BAD_REQUEST));
     }
 
 }
