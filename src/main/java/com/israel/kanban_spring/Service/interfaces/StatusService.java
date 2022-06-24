4e23db111231aac4fe23e82b6902534b0be53a6e
israel <israelmessias2016@outlook.com>
2022-06-24 14:49:37 -0300

    Interface de Service Status

--- src/main/java/com/israel/kanban_spring/Service/interfaces/StatusService.java
+++ src/main/java/com/israel/kanban_spring/Service/interfaces/StatusService.java
@@ -1,2 +1,9 @@
-package com.israel.kanban_spring.Service.interfaces;public interface StatusService {
+package com.israel.kanban_spring.Service.interfaces;
+
+import com.israel.kanban_spring.model.entity.Status;
+
+import java.util.Optional;
+
+public interface StatusService {
+    Optional<Status>obterStatusPorId(Integer id);
 }
