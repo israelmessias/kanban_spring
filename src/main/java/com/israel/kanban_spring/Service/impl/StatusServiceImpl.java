4417d1d705b8a0a4d476c8fa77a6a239bf3d1bc6
israel <israelmessias2016@outlook.com>
2022-06-24 14:50:26 -0300

    obterStatusPorId implementado

--- src/main/java/com/israel/kanban_spring/Service/impl/StatusServiceImpl.java
+++ src/main/java/com/israel/kanban_spring/Service/impl/StatusServiceImpl.java
@@ -1,2 +1,17 @@
-package com.israel.kanban_spring.Service.impl;public class StatusServiceImpl {
+package com.israel.kanban_spring.Service.impl;
+
+import com.israel.kanban_spring.Service.interfaces.StatusService;
+import com.israel.kanban_spring.model.entity.Status;
+import com.israel.kanban_spring.model.repository.StatusRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+
+public class StatusServiceImpl implements StatusService {
+    @Autowired
+    StatusRepository repository;
+    @Override
+    public Optional<Status> obterStatusPorId(Integer id) {
+        return repository.findById(id);
+    }
 }
